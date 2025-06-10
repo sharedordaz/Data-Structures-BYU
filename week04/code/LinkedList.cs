3f16292540b5b47f16ce8885c03c99c7aff4b0a2
SharedOrdaz <sharedordaz@live.com.mx>
2025-06-09 19:49:16 -0600

    InsertTail()

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -30,10 +30,25 @@ public class LinkedList : IEnumerable<int>
     /// <summary>
     /// Insert a new node at the back (i.e. the tail) of the linked list.
     /// </summary>
-    public void InsertTail(int value)
+public void InsertTail(int value)
+{
+    // Create new node
+    Node newNode = new(value);
+
+    // If the list is empty, point both head and tail to the new node
+    if (_tail is null)
     {
-        // TODO Problem 1
+        _head = newNode;
+        _tail = newNode;
     }
+    // If the list is not empty, only the tail will be affected
+    else
+    {
+        _tail.Next = newNode; // Connect the current tail to the new node
+        newNode.Prev = _tail; // Connect the new node to the current tail
+        _tail = newNode; // Update the tail to point to the new node
+    }
+}
 
 
     /// <summary>
